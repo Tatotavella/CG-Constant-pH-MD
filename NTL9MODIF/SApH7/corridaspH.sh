26c93ec941addd1a89aed2f6c2d37eed78ea0c16
Tatotavella <tavellafran@gmail.com>
2017-03-09 18:26:59 -0300

    Analisis de modelos y scripts de calculo de pkas

--- NTL9MODIF/SApH7/corridaspH.sh
+++ NTL9MODIF/SApH7/corridaspH.sh
@@ -5,26 +5,48 @@ freqMC=5
 
 pKa1=4.5
 pKa2=5.0
+
+totalResCharged=15
+columns=$((totalResCharged+1))
 in=testeo
-MCfile=MC.data
-r1=22
-r2=37
-r1q=-1
-r2q=-1
+MCfile=MC.input
 MCres=MC.result
-out=res.txt
-rm $out $r2.txt $r1.txt
+MCdeprot=MC.deprot
+out=result
 
-for i in $(seq 2 2 12); do
 
-sed "s/pKa1/$pKa1/g" "$MCfile".bk | sed "s/pKa2/$pKa2/g" | sed "s/pH/$i/g" | sed "s/freqMC/$freqMC/g" > $MCfile
-sed "s/PH/$i/g" "$in".in.bk > "$in".in
-./lmp_serial < "$in".in
-total1=`awk -v var=$r1 'NR>1{if ($1==var) print $2,$3}' $MCres | wc | awk '{print $1}'`
-deprot1=`awk -v var=$r1 -v vare=$r1q 'NR>1{if ($1==var && $2==vare) print $2,$3}' $MCres | wc | awk '{print $1}'`
-total2=`awk -v var=$r2 'NR>1{if ($1==var) print $2,$3}' $MCres | wc | awk '{print $1}'`
-deprot2=`awk -v var=$r2 -v vare=$r2q 'NR>1{if ($1==var && $2==vare) print $2,$3}' $MCres | wc | awk '{print $1}'`
-#echo fd=$((deprot/total))
-echo $i $deprot1 >> "$r1".txt
-echo $i $deprot2 >> "$r2".txt
+rm $out.txt
+
+write="pH"
+for j in $(seq 1 1 $totalResCharged); do
+res=`awk 'NR>3{print $1}' $MCfile | awk -v var=$j 'FNR==var{print}'`
+echo $res
+write+="|$res"
 done
+
+echo -e $write >> "$out".txt 
+
+lineEnds=""
+
+for i in $(seq 1 1 14); do
+write=""
+write+="$i"
+sed "s/freqMC/$freqMC/g" "$MCfile".bk | sed "s/pH/$i/g"  > $MCfile
+#sed "s/PH/$i/g" "$in".in.bk > "$in".in
+./lmp_serial < "$in".in
+
+endofline=`awk 'END{print}' $MCdeprot`
+lineEnds+="\n$endofline"
+
+
+for j in $(seq 2 1 $columns); do
+deprot=`awk 'END{print}' $MCdeprot | awk -v var=$j '{print $var/($1+1)}'`
+write+="|$deprot"
+done
+
+echo $write >> "$out".txt 
+
+done
+echo -e $lineEnds
+column -s"|" -t "$out".txt >> salida.txt
+mv salida.txt "$out".txt
